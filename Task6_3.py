4fe1eac32757316629e904368b015144313c48bf
Andrey Evseev <a-evseev@yandex.ru>
2022-07-26 19:22:48 +0300

    Решение задания 3

--- Task6_3.py
+++ Task6_3.py
@@ -1,3 +1,6 @@
 # Задание 3. Дана последовательность чисел. 
 # Получить список уникальных элементов заданной последовательности.
 
+numbers_list = [1, 2, 3, 5, 1, 5, 3, 10]
+result_list = list(filter(lambda a: numbers_list.count(a) == 1, numbers_list))
+print(f'Для последовательности {numbers_list} список уникальных элементов => {result_list}')
