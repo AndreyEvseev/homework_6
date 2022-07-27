5e3db680f901f49d1b5f1a9dd0f438b0b7199659
Andrey Evseev <a-evseev@yandex.ru>
2022-07-27 19:52:26 +0300

    Решение задания 2_2

--- Task6_2_1.py
+++ Task6_2_1.py
@@ -1,6 +1,11 @@
 # Задание 2_1. Дана последовательность чисел. 
 # Получить список уникальных элементов заданной последовательности.
 
-numbers_list = [1, 2, 3, 5, 1, 5, 3, 10]
+import random
+
+n = int(input('Количество элементов списка: '))
+b1 = int(input('Граница 1 диапазона значений элементов списка: '))
+b2 = int(input('Граница 2 диапазона значений элементов списка: '))
+numbers_list = [random.randint(min(b1, b2), max(b1, b2)) for i in range(n)]
 result_list = list(filter(lambda a: numbers_list.count(a) == 1, numbers_list))
-print(f'Для последовательности {numbers_list} список уникальных элементов => {result_list}')
+print(f'Для последовательности {numbers_list}, \n   список уникальных элементов => {result_list}')
